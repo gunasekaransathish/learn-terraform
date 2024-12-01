23b5bd7057cae637d4811fc5dad2bdddfc51430c
gunasekaransathish <sathish.gunasekaran@gmail.com>
2024-12-01 17:09:26 +0530

    added remaining services

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,9 +1,170 @@
-
-resource "aws_instance" "web" {
+resource "aws_instance" "frontend" {
   ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t2.micro"
-
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
   tags = {
-    Name = "HelloWorld"
+    Name = "frontend"
   }
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z02249652EM5BAO495DZ1"
+  name    = "frontend-dev.harsharoboticshop.online"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.frontend.private_ip]
+}
+
+resource "aws_instance" "mongodb" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
+  tags = {
+    Name = "mongodb"
+  }
+}
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z02249652EM5BAO495DZ1"
+  name    = "mongodb-dev.harsharoboticshop.online"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.mongodb.private_ip]
+}
+
+resource "aws_instance" "catalogue" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
+  tags = {
+    Name = "catalogue"
+  }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z02249652EM5BAO495DZ1"
+  name    = "catalogue-dev.harsharoboticshop.online"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.catalogue.private_ip]
+}
+
+resource "aws_instance" "redis" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
+  tags = {
+    Name = "redis"
+  }
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = "Z02249652EM5BAO495DZ1"
+  name    = "redis-dev.harsharoboticshop.online"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.redis.private_ip]
+}
+
+resource "aws_instance" "user" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
+  tags = {
+    Name = "user"
+  }
+}
+
+resource "aws_route53_record" "user" {
+  zone_id = "Z02249652EM5BAO495DZ1"
+  name    = "user-dev.harsharoboticshop.online"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.user.private_ip]
+}
+
+resource "aws_instance" "cart" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
+  tags = {
+    Name = "cart"
+  }
+}
+
+resource "aws_route53_record" "cart" {
+  zone_id = "Z02249652EM5BAO495DZ1"
+  name    = "cart-dev.harsharoboticshop.online"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.cart.private_ip]
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z02249652EM5BAO495DZ1"
+  name    = "mysql-dev.harsharoboticshop.online"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.mysql.private_ip]
+}
+
+resource "aws_instance" "shipping" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
+  tags = {
+    Name = "shipping"
+  }
+}
+
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z02249652EM5BAO495DZ1"
+  name    = "shipping-dev.harsharoboticshop.online"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.shipping.private_ip]
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z02249652EM5BAO495DZ1"
+  name    = "rabbitmq-dev.harsharoboticshop.online"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.rabbitmq.private_ip]
+}
+
+
+resource "aws_instance" "payment" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0be95cb0c3fef9448"]
+  tags = {
+    Name = "payment"
+  }
+}
+
+resource "aws_route53_record" "payment" {
+  zone_id = "Z02249652EM5BAO495DZ1"
+  name    = "payment-dev.harsharoboticshop.online"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.payment.private_ip]
 }
